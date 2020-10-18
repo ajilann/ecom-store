105b7f424b294c37aa6bf6cc99a16f758102ffef
ajilann <ajilan.nazarudeen@gmail.com>
2020-10-18 22:12:43 +0530

    the home and product components are added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import "./App.css";
+import Home from "./Home";
 
 function App() {
   return (
     //BEM conversion
     <div className="app">
       <Header />
+      <Home />
     </div>
   );
 }
