38b17496538a45b2299f1656f53cde607ec93b1c
ajilann <ajilan.nazarudeen@gmail.com>
2020-10-19 13:57:00 +0530

    the react router is added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React from "react";
 import Header from "./Header";
 import "./App.css";
 import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     //BEM conversion
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <h1>I am a Checout</h1>
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
