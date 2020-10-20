8cbf7e8987e7c927105e6ccf24bd476ba427158b
ajilann <ajilan.nazarudeen@gmail.com>
2020-10-20 12:59:06 +0530

    added the checkout page and the subtotal components for the product

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import "./App.css";
 import Home from "./Home";
+import Checkout from "./Checkout";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
         <Switch>
           <Route path="/checkout">
             <Header />
-            <h1>I am a Checout</h1>
+            <Checkout />
           </Route>
           <Route path="/">
             <Header />
