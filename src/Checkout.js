59a5a6995c40cfb46e5b2842185699fc0be71f2e
ajilann <ajilan.nazarudeen@gmail.com>
2020-10-21 13:48:11 +0530

    the products are been added to the basket

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,8 +1,12 @@
 import React from "react";
 import "./Checkout.css";
 import Subtotal from "./Subtotal";
+import { useStateValue } from "./StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
 
 function Checkout() {
+  const [{ basket }, dispatch] = useStateValue();
+
   return (
     <div className="checkout">
       {/* checkout left side*/}
@@ -16,8 +20,17 @@ function Checkout() {
         <div>
           <h2 className="checkout__title">Your shopping Basket</h2>
 
-          {/* BasketItem*/}
-          {/* BasketItem*/}
+          {basket.map((item) => (
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
+          {/* CheckoutItem*/}
+          {/* CheckoutItem*/}
         </div>
       </div>
 
