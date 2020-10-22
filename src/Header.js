049508c254444d5f59d4655b0f6708452d0b63f6
ajilann <ajilan.nazarudeen@gmail.com>
2020-10-22 11:59:04 +0530

    the user is now able to sign in or sign out

--- src/Header.js
+++ src/Header.js
@@ -4,9 +4,16 @@ import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 import { Link } from "react-router-dom";
 import { useStateValue } from "./StateProvider";
+import {auth} from "./firebase";
 
 function Header() {
-  const [{ basket }, dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
+
+  const handleAuthentication = () => {
+    if (user) {
+      auth.signOut();
+    }
+  }
 
   return (
     <div className="header">
@@ -21,11 +28,14 @@ function Header() {
         <input className="header__searchInput" type="text" />
         <SearchIcon className="header__searchIcon" />
       </div>
+
+
       <div className="header__nav">
         <Link to="/login">
-          <div className="header__options">
+          <div onClick= {handleAuthentication} 
+          className="header__options">
             <span className="header__optionsLineOne">Hello Guest</span>
-            <span className="header__optionsLineTwo">Sign In</span>
+            <span className="header__optionsLineTwo">{user? 'Sign Out' : 'Sign In'}</span>
           </div>
         </Link>
         <div className="header__options">
